68d66eb48d484f09b7b25612d8f6f91c0759fcb4
Dmitriy Shingarey <shingarey@gmail.com>
2022-12-22 08:30:20 +0100

    clean week5

--- white_belt/solutions/week5_final_project/project.cpp
+++ white_belt/solutions/week5_final_project/project.cpp
@@ -1,147 +1,169 @@
 #include <iostream>
-#include <sstream>
 #include <map>
 #include <set>
+#include <sstream>
 #include <string>
 #include <vector>
 
 using namespace std;
 
-map<string, int> Command = {{"Add",1},{"Del",2},{"Find",3},{"Print",4}};
+map<string, int> Command = {{"Add", 1}, {"Del", 2}, {"Find", 3}, {"Print", 4}};
 
-class Date {
+class Date
+{
 public:
-  int GetYear() const{
-      return year;
+  int GetYear() const
+  {
+    return year;
   }
-  int GetMonth() const{
-     return month;
+  int GetMonth() const
+  {
+    return month;
   }
-  int GetDay() const{
-     return day;
+  int GetDay() const
+  {
+    return day;
   }
 
-  void NewDate(int year_, int month_, int day_) {
-     year = year_;
-     month = month_;
-     day = day_;
-   }
+  void NewDate(int year_, int month_, int day_)
+  {
+    year = year_;
+    month = month_;
+    day = day_;
+  }
 
-  private:
-    int year;
-    int month;
-    int day;
+private:
+  int year;
+  int month;
+  int day;
 };
 
-bool operator<(const Date& lhs, const Date& rhs){
+bool operator<(const Date &lhs, const Date &rhs)
+{
   vector<int> lhs_key{lhs.GetYear(), lhs.GetMonth(), lhs.GetDay()};
   vector<int> rhs_key{rhs.GetYear(), rhs.GetMonth(), rhs.GetDay()};
   return (lhs_key < rhs_key);
 }
 
-class Database {
+class Database
+{
 public:
-  void AddEvent(const Date& dato, const string& event){
+  void AddEvent(const Date &dato, const string &event)
+  {
     db[dato].insert(event);
   }
-  bool DeleteEvent(const Date& date, const string& event){
-    auto N =  db.at(date).size();
+  bool DeleteEvent(const Date &date, const string &event)
+  {
+    auto N = db.at(date).size();
     cout << "Size is: " << N << endl;
-    if (N > 0 )
+    if (N > 0)
     {
       if (event.size() != 0)
-        {
-          db.at(date).erase(event);
-          cout << "Deleted successfully." << endl;
-          return true;
-        }
+      {
+        db.at(date).erase(event);
+        cout << "Deleted successfully." << endl;
+        return true;
+      }
       else
       {
         db.erase(date);
-        cout << "Deleted " + to_string(N)+ " events." << endl;
+        cout << "Deleted " + to_string(N) + " events." << endl;
         return true;
-      } 
+      }
     }
-    else {
-        cout << "Event not found" << endl;
-        return false;
-    }    
+    else
+    {
+      cout << "Event not found" << endl;
+      return false;
+    }
   }
 
-  void Find(const Date& date) const{
+  void Find(const Date &date) const
+  {
     for (auto event : db.at(date))
     {
       cout << event << endl;
     }
-    
   }
-  
-  void Print() const{
+
+  void Print() const
+  {
     for (auto i : db)
     {
       for (auto event : i.second)
       {
-        cout  << i.first.GetYear() << "-"
-              << i.first.GetMonth() << "-"
-              << i.first.GetDay() << " "
-              << event <<endl;
-      }  
+        cout << i.first.GetYear() << "-"
+             << i.first.GetMonth() << "-"
+             << i.first.GetDay() << " "
+             << event << endl;
+      }
     }
   }
 
 private:
-   std::map<Date, set<string>> db; 
+  std::map<Date, set<string>> db;
 };
 
-void ValidateAndCreateDate(istream & ss_command, Date & date){
-    int year;
-    int month = 0;
-    int day = 0;
-    if (ss_command >> year){
-      ss_command.ignore(1);
-      if (ss_command >> month){
-        if (month < 1 || month > 12){
-            cout << "Month value is invalid: " + to_string(month) << endl;
-            exit(3);
-        }
-        ss_command.ignore(1);
-        if (ss_command >> day){
-               if (day<1 || day > 31)
-                {
-                  cout << "Day value is invalid: " + to_string(day) << endl;
-                  exit(3);
-                }
-            date.NewDate(year, month, day);
-        } 
+void ValidateAndCreateDate(istream &ss_command, Date &date)
+{
+  int year;
+  int month = 0;
+  int day = 0;
+  if (ss_command >> year)
+  {
+    ss_command.ignore(1);
+    if (ss_command >> month)
+    {
+      if (month < 1 || month > 12)
+      {
+        cout << "Month value is invalid: " + to_string(month) << endl;
+        exit(3);
       }
-      else {
-        cout << "Wrong date format." << endl;
+      ss_command.ignore(1);
+      if (ss_command >> day)
+      {
+        if (day < 1 || day > 31)
+        {
+          cout << "Day value is invalid: " + to_string(day) << endl;
+          exit(3);
+        }
+        date.NewDate(year, month, day);
       }
     }
+    else
+    {
+      cout << "Wrong date format." << endl;
+    }
+  }
 }
 
-int main() {
-  Database db;  
+int main()
+{
+  Database db;
   string command;
   cout << "Enter command: " << endl;
-  while (getline(cin, command)) {
+  while (getline(cin, command))
+  {
     istringstream ss_command(command);
     string c;
     ss_command >> c;
     switch (Command[c])
     {
-    case 1: { // add date to databse
+    case 1:
+    { // add date to databse
       Date date;
-      ValidateAndCreateDate(ss_command,date);
+      ValidateAndCreateDate(ss_command, date);
       ss_command.ignore(1);
       string event = "";
       ss_command >> event;
-      if (event.size() != 0){
+      if (event.size() != 0)
+      {
         db.AddEvent(date, event);
       }
-      break; 
-      }
-    case 2: { // add date to databse
+      break;
+    }
+    case 2:
+    { // add date to databse
       Date date;
       ValidateAndCreateDate(ss_command, date);
       ss_command.ignore(1);
@@ -149,20 +171,23 @@ int main() {
       ss_command >> event;
       cout << "Deleting event(s): " << endl;
       db.DeleteEvent(date, event);
-      break; 
-      }
-    case 3: { // find events for a date
+      break;
+    }
+    case 3:
+    { // find events for a date
       Date date;
       ValidateAndCreateDate(ss_command, date);
       cout << "Searching for event(s): " << endl;
       db.Find(date);
-      break; 
-      }
-    case 4: { // print all dates and events
+      break;
+    }
+    case 4:
+    { // print all dates and events
       db.Print();
-      break; 
-      }
-    default:{
+      break;
+    }
+    default:
+    {
       cout << "Unknown command: " + c << endl;
       cout << "Enter command: " << endl;
       break;
--- white_belt/solutions/week5_final_project/project2.cpp
+++ white_belt/solutions/week5_final_project/project2.cpp
@@ -1,74 +1,82 @@
 #include <iostream>
-#include <sstream>
 #include <map>
 #include <set>
+#include <sstream>
 #include <string>
 
 using namespace std;
 
-map<string, int> Command = {{"Add",1},{"Del",2},{"Find",3},{"Print",4}};
+map<string, int> Command = {{"Add", 1}, {"Del", 2}, {"Find", 3}, {"Print", 4}};
 
-class Date {
+class Date
+{
 public:
   int GetYear() const;
   int GetMonth() const;
   int GetDay() const;
 
-    void NewDate(int & year_,int & month_,int & day_){
+  void NewDate(int &year_, int &month_, int &day_)
+  {
     year = year_;
     month = month_;
     day = day_;
   }
-  private:
-    int year;
-    int month;
-    int day;
+
+private:
+  int year;
+  int month;
+  int day;
 };
 
-bool operator<(const Date& lhs, const Date& rhs){
-    return lhs < rhs;
+bool operator<(const Date &lhs, const Date &rhs)
+{
+  return lhs < rhs;
 }
 
-class Database {
+class Database
+{
 public:
-    void AddEvent(const Date& date, const string& event){
-    //cout << date.GetDay() << endl;
-    //set<string> myset;
-    //myset.insert(event);
+  void AddEvent(const Date &date, const string &event)
+  {
+    // cout << date.GetDay() << endl;
+    // set<string> myset;
+    // myset.insert(event);
     if (Database::db.find(date) == Database::db.end())
     {
       Database::db[date] = "test";
     }
-     //event;
+    // event;
   }
-  bool DeleteEvent(const Date& date, const string& event);
-  int  DeleteDate(const Date& date);
+  bool DeleteEvent(const Date &date, const string &event);
+  int DeleteDate(const Date &date);
 
-  /* ??? */ //Find(const Date& date) const;
-  
+  /* ??? */ // Find(const Date& date) const;
+
   void Print() const;
 
-  private:
-    std::map<Date, std::string> db; 
+private:
+  std::map<Date, std::string> db;
 };
 
-int main() {
+int main()
+{
   Database db;
-    
-    string command;
-  
+
+  string command;
+
   cout << "Enter command: " << endl;
 
-  while (getline(cin, command)) {
+  while (getline(cin, command))
+  {
     istringstream ss_command(command);
     string c;
     ss_command >> c;
     switch (Command[c])
     {
-    case 1:  // add date to databse
-      int year =0;
-      int month=0;
-      int day=0;
+    case 1: // add date to databse
+      int year = 0;
+      int month = 0;
+      int day = 0;
       ss_command >> year;
       ss_command.ignore(1);
       ss_command >> month;
@@ -76,20 +84,20 @@ int main() {
       ss_command >> day;
       Date date;
       date.NewDate(year, month, day);
-      //ValidateAndCreateDate(ss_command,date);
+      // ValidateAndCreateDate(ss_command,date);
       ss_command.ignore(1);
       string event = "";
       ss_command >> event;
       db.AddEvent(date, event);
-      //break; 
-      
-   //   default:{
-   //   cout << "Unknown command: " + c << endl;
-   //   cout << "Enter command: " << endl;
-   //   break;
-   // }
+      // break;
+
+      //   default:{
+      //   cout << "Unknown command: " + c << endl;
+      //   cout << "Enter command: " << endl;
+      //   break;
+      // }
     }
-    }
+  }
 
   return 0;
 }
